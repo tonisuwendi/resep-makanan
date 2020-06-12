df7ce28243f921d9d999c656581dca9af9c15f1f
tonisuwendi <tonisuwendi2002@gmail.com>
2020-06-12 10:00:06 -0700

    Initial Commit

---
+++ Ricep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ricep
+//
+//  Created by mac on 12/06/20.
+//  Copyright © 2020 Tonsu Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
